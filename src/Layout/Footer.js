b8c70e79f501f3891601a9027e155b5d1d48f144
unknown <ysujin1001@gmail.com>
2025-07-31 12:50:59 +0900

    footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -2,8 +2,35 @@ function Footer() {
   return (
     <>
       <footer>
-        <div></div>
-        <div></div>
+        <div>
+          <ul>
+            <li className="footer-li-tilte">
+              <a href="/company">company</a>
+            </li>
+            <li>
+              <a href="/company">한눈에 보기</a>
+            </li>
+            <li>
+              <a href="/company">스타벅스 사명</a>
+            </li>
+            <li>
+              <a href="/company">스타벅스 소개</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className="footer-li-tilte">
+              <a href="/">coporate sales</a>
+            </li>
+            <li>
+              <a href="/">단체 및 기업 구매 안내</a>
+            </li>
+            <li>
+              <a href="/">단체 주문 배달 안내</a>
+            </li>
+          </ul>
+        </div>
       </footer>
     </>
   );
